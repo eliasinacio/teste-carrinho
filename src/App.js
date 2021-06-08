2dd5b03b9692bf5e82222284e2b7df6e32362daf
eliasinacio <el1as.1nac1o.ch@gmail.com>
2021-06-08 10:13:53 -0300

    Styles finished

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 import CardItem from './CardItem'
 
+import './styles/App.scss';
+
 class App extends React.Component {
   render () {
     return (
-      <CardItem />
+      <div className="total-area">
+        <main className="cards-container">
+          <h1>Click/touch to Select</h1>
+          <CardItem />
+        </main>
+      </div>
     )
   }
 }
--- src/CardItem.js
+++ src/CardItem.js
@@ -6,10 +6,12 @@ class CardItem extends React.Component {
   render () {
     return (
       <div className="card-item">
-        <img className="item-img" src="https://a" alt="imagem BomBom Garoto"></img>
-        <div className="item-info">
-          <h1 className="item-name">BomBom Garoto</h1>
-          <h2 className="item-price">R$ 3,00</h2>
+        <img className="card-img" 
+          src="http://codeby.vteximg.com.br/arquivos/ids/159959-800-1029/truffon-meio-amargo.png?v=636930938547630000"
+          alt="imagem BomBom Garoto"></img>
+        <div className="card-info">
+          <h1 className="card-name">BomBom Garoto</h1>
+          <h2 className="card-price">R$ 3,00</h2>
         </div>
       </div>
     )
